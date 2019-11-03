a6bf37d1db5e241793d8c113b56801ef5c2b9f6c
VirtualVortex <mcoates111@gmail.com>
2019-11-03 15:14:33 +0000

    Create CrystalBreak.cs

---
+++ CrystalBreak.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalBreak : MonoBehaviour {
+
+    public CrystalCount winPortal;
+
+    private void OnDestroy()
+    {
+        winPortal.CrystalBroken();
+    }
+}
