fe9c788395e4a4144c035a1023f380c6f91aa306
VirtualVortex <mcoates111@gmail.com>
2019-11-03 15:16:13 +0000

    Create KeyScript(old).cs

---
+++ KeyScript(old).cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScriptOld : MonoBehaviour {
+    public GameObject targetObject;
+    public bool haveKey;
+
+    void Start()
+    {
+        if (haveKey)
+        {
+            targetObject.SetActive(false);
+            Destroy(gameObject);
+        }
+            
+    }
+
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+
+        if (targetObject == null)
+            return;
+        if (Other.tag != "Player")
+            return;
+        targetObject.SetActive(false);
+        Destroy(gameObject);
+        haveKey = true;
+    }
+}
