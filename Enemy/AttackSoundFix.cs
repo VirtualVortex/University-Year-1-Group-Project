be52fa0662a11927fb0fd6af0b928deac496ca3c
VirtualVortex <mcoates111@gmail.com>
2019-11-03 15:15:30 +0000

    Create AttackSoundFix.cs

---
+++ Enemy/AttackSoundFix.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSoundFix : MonoBehaviour {
+
+    public void PlayAboutToAttack()
+    {
+        GetComponentInParent<EnemyAudioScript>().PlayAboutToAttack();
+    }
+
+    public void PlayAttackSound()
+    {
+        GetComponentInParent<EnemyAudioScript>().PlayAttack();
+    }
+
+    public void HitPlayer()
+    {
+        GetComponentInParent<TestAI>().hitPlayer();
+    }
+}
